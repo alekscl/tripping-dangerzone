477923e3c805d0138d729f30a8294f4b38d124d7
Alejandro Carmona Ligeon <alekscl@gmail.com>
2014-04-13 08:22:10 +0200

    Adding a dynamic route

--- flask-hello-world/app.py
+++ flask-hello-world/app.py
@@ -5,6 +5,10 @@ from flask import Flask
 # create the application object
 app = Flask(__name__)
 
+
+app.config["DEBUG"] = True
+
+
 # use decorators to link the function to a url
 
 
@@ -12,6 +16,13 @@ app = Flask(__name__)
 @app.route('/hello')
 def hello_world():
     return "Hello world"
+
+# dynamic route
+
+
+@app.route("/test/<search_query>")
+def search(search_query):
+    return search_query
 
 # start the development server using the run method
 if __name__ == "__main__":
