44ad29d89a9485645db16923c75e8cc44c44ba96
Alejandro Carmona Ligeon <alekscl@gmail.com>
2014-04-28 17:17:08 +0200

    Script if fixed after using pdb.

--- debugging/pdb_ex.py
+++ debugging/pdb_ex.py
@@ -10,15 +10,17 @@ random2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
 os.system("clear")
 while True:
-    print "To exit this game type: 'exit'"
     pdb.set_trace()
-    answer = raw_input(
-        "What is {} times {}? ".format(choice(random2), choice(random1)))
+    print "To exit this game type: 'exit'"
+    num1 = choice(random2)
+    num2 = choice(random1)
+    answer = raw_input("What is {} times {}? ".format(num1, num2))
 
     if answer == 'exit':
         print "Now exiting game!"
         sys.exit()
-    elif answer == choice(random2) * choice(random1):
+    elif int(answer) == num1 * num2:
         print "Correct!!"
+        break
     else:
         print "Wrong!!!"
