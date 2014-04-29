01765cccd6bec534327783c12d40f3e3b701e49d
Alejandro Carmona Ligeon <alekscl@gmail.com>
2014-04-29 13:12:17 +0200

    Another example

---
+++ client-side/clientc.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import requests
+
+
+def main():
+    url = "http://httpbin.org/post"
+    data = {'fname': 'Michael', 'lname': 'Herman'}
+    r = requests.post(url, data=data)
+    print r
+
+
+if __name__ == "__main__":
+    main()
