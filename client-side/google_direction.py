5686b0464745b3bf7b7f9b60756b51eb232ca7e4
Alejandro Carmona Ligeon <alekscl@gmail.com>
2014-04-30 12:59:19 +0200

    Silly script that tells how to go from caleruega to orense

---
+++ client-side/google_direction.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import json
+import requests
+
+
+def main():
+    porigin = "origin=Calle+de+Caleruega+Madrid"
+    pdestination = "destination=Calle+Orense+Madrid"
+    psensor = "sensor=false"
+    url = "http://maps.googleapis.com/maps/api/directions/json?" + \
+        porigin + "&" + pdestination + "&" + psensor
+    response = requests.get(url)
+
+    with open("response.json", "wb") as f:
+        f.write(response.content)
+
+    output = json.load(open("response.json", "rb"))
+
+    for route in output["routes"]:
+        for leg in route["legs"]:
+            for step in leg["steps"]:
+                print step["distance"]
+
+
+if __name__ == "__main__":
+    main()
