28f5be9eec23873cbe9ea4a77f9a60c3e796ef44
Alejandro Carmona Ligeon <alekscl@gmail.com>
2014-05-04 23:26:22 +0200

    Fixing settings.py, forgot to add TEMPLATE_PATH to the TEMPLATE_DIRS and adding a new url in the urls.py file

--- django/hello_world/hello_world/settings.py
+++ django/hello_world/hello_world/settings.py
@@ -6,6 +6,7 @@ PROJECT_PATH = os.path.join(SETTINGS_DIR, os.pardir)
 PROJECT_PATH = os.path.abspath(PROJECT_PATH)
 TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
 #print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+print TEMPLATE_PATH
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -122,6 +123,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates"
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
 )
 
 INSTALLED_APPS = (
--- django/hello_world/hw/urls.py
+++ django/hello_world/hw/urls.py
@@ -4,4 +4,5 @@ from hw import views
 urlpatterns = patterns(
     '',
     url(r'^hello/$', views.hello_view, name='hello_view'),
+    url(r'^better/$', views.better_hello, name='better_hello'),
 )
