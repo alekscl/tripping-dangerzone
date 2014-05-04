5269ee607f2854e529fd695667c3ae6dae2129e5
Alejandro Carmona Ligeon <alekscl@gmail.com>
2014-05-04 23:17:01 +0200

    Adding a new function and modifying the first.

--- django/hello_world/hw/views.py
+++ django/hello_world/hw/views.py
@@ -1,6 +1,25 @@
 # Create your views here.
 from django.shortcuts import HttpResponse
+from django.template import Context, loader
+from datetime import datetime
 
 
 def hello_view(request):
-    return HttpResponse("Hello world")
+    return HttpResponse(
+        """
+        <html>
+        <head>
+        <title>Hello world</title>
+        </head>
+        <body>
+        <h2>Hello world</h2>
+        </body>
+        </html>
+        """
+    )
+
+
+def better_hello(request):
+    t = loader.get_template("hw/betterhello.html")
+    c = Context({'current_time': datetime.now()})
+    return HttpResponse(t.render(c))
