5ae5214e4fc8e2e2079b981e3a54258b56e2dbfb
Alejandro Carmona Ligeon <alekscl@gmail.com>
2014-05-05 12:06:24 +0200

    Fixing this two files

--- django/bloggy/blog/views.py
+++ django/bloggy/blog/views.py
@@ -7,5 +7,5 @@ from django.template import Context, loader
 def index(request):
     latest_posts = Post.objects.all()
     t = loader.get_template('blog/index.html')
-    c = Context({'lastes_posts': latest_posts})
+    c = Context({'latest_posts': latest_posts})
     return HttpResponse(t.render(c))
