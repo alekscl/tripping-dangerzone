97f24fd475677aac83a65b9aecda564415f52106
Alejandro Carmona Ligeon <alekscl@gmail.com>
2014-05-05 10:37:22 +0200

    Adding __unicode__ function  to models.py

--- django/bloggy/blog/models.py
+++ django/bloggy/blog/models.py
@@ -4,6 +4,10 @@ from django.db import models
 
 
 class Post(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    title = models.CharField(max_length=100)
-    content = models.TextField()
+    created_at = models.DateTimeField(
+        auto_now_add=True, verbose_name="Created at")
+    title = models.CharField(max_length=100, verbose_name="Title")
+    content = models.TextField(verbose_name="Content")
+
+    def __unicode__(self):
+        return self.title
